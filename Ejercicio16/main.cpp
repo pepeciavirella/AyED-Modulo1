d00fc542cfa167ecac2941c568f6e858f8aee874
pepeciavirella <pep.ciavirella@gmail.com>
2018-06-06 14:57:31 -0300

    Ajustes en el ejercicio 16: ahora es con while y no do-while
    Signed-off-by: pepeciavirella <pep.ciavirella@gmail.com>

--- Ejercicio16/main.cpp
+++ Ejercicio16/main.cpp
@@ -5,17 +5,33 @@
 /// no sea negativo. Informar la cantidad de valores ingresados
 /// Autor: Giuseppe Ciavirella
 
+//NOTA: La idea era hacerlo con un ciclo 0-n, es decir while, y hacer su versión do-while más adelante
+
 using namespace std;
 
 int main()
 {
     float numero;
     int cantidad = 0;
-    do{
+    cout << "Ingrese un numero no negativo, o bien uno negativo para detener el ingreso" << endl;
+    cin >> numero;
+    while(numero >= 0)
+    {
+        cantidad++; //HIPÓTESIS: El negativo que frene el ingreso no cuenta
         cout << "Ingrese un numero no negativo, o bien uno negativo para detener el ingreso" << endl;
         cin >> numero;
-        cantidad++;  //El numero negativo que frene el ingreso también contará entre la cantidad
-    }while(numero >= 0);
-    cout << "Se ingresaron, en total, " << cantidad << " numeros" << endl;
+    }
+    if(cantidad == 0)
+    {
+        cout << "No se ingresaron numeros no negativos" << endl;
+    }
+    else if(cantidad == 1)
+    {
+        cout << "Se ingreso un solo numero no negativo" << endl;
+    }
+    else
+    {
+        cout << "Se ingresaron " << cantidad << " numeros no negativos" << endl;
+    }
     return 0;
 }
